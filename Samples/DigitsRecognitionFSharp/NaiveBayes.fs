b9258519f0ca521ba98b9335c04461a0fa0d1e77
alina.gryaznova <alina.gryaznova@sigmaukraine.com>
2015-10-09 19:29:18 +0300

    adds classifier

---
+++ Samples/DigitsRecognitionFSharp/NaiveBayes.fs
@@ -0,0 +1,33 @@
+﻿namespace NaiveBayes
+
+module Classifier =
+
+    let hello name = printfn "Hello, %s" name
+
+    type Token = string
+    type Tokenizer = string -> Token Set
+    type TokenizedDocument = Token Set
+
+    type DocsGroup =
+        { Proportion:float;  
+          TokenFrequiencies:Map<Token, float>}
+
+    let tokenScore (group:DocsGroup) (token:Token) =
+        if group.TokenFrequiencies.ContainsKey token
+        then log group.TokenFrequiencies.[token]
+        else 0.0
+
+    let score (document:TokenizedDocument) (group:DocsGroup) =
+        let scoreToken = tokenScore group
+        log group.Proportion +
+        (document |> Seq.sumBy scoreToken)
+
+    let classify (groups:(_*DocsGroup)[]) (tokenizer:Tokenizer) (txt:string) =
+        let tokenized = tokenizer txt
+        groups
+        |> Array.maxBy(fun(label, group) -> score tokenized group)
+        |> fst
+
+
+
+
