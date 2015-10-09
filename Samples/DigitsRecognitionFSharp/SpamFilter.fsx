764265362b2d5fdf1d10014ae364dec89543ea0c
alina.gryaznova <alina.gryaznova@sigmaukraine.com>
2015-10-09 18:54:33 +0300

    adds initial code for spam recognition

---
+++ Samples/DigitsRecognitionFSharp/SpamFilter.fsx
@@ -0,0 +1,36 @@
+﻿open System.IO
+
+type DocType = 
+    |Ham
+    |Spam
+
+let parseDocType(label:string)=
+    match label with
+    |"ham" -> Ham
+    |"spam" ->  Spam
+    |_ -> failwith "Unknown label"
+
+let parseline (line:string) =
+    let split = line.Split('\t')
+    let label = split.[0] |> parseDocType
+    let message = split.[1]
+    (label, message)
+
+let fileName = "SMSSpamCollection"
+let path = __SOURCE_DIRECTORY__ + @"..\..\Data\" + fileName
+
+let dataset = 
+    File.ReadAllLines path
+    |> Array.map parseline
+
+let spamWithFree = 
+    dataset
+    |> Array.filter (fun(docType, _) -> docType = Spam)
+    |> Array.filter (fun(_, content) -> content.Contains "FREE")
+    |> Array.length
+
+let hamWithFree = 
+    dataset
+    |> Array.filter (fun(docType, _) -> docType = Ham)
+    |> Array.filter (fun(_, content) -> content.Contains "FREE")
+    |> Array.length
