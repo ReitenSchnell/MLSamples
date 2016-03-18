5cd0df5ee3aad7bd53dbaafd88ff280f1bd0ddbe
alina.gryaznova <alina.gryaznova@sigmaukraine.com>
2016-03-18 18:42:40 +0200

    adds models with more features

--- Samples/Regression/LinearAlgebra.fsx
+++ Samples/Regression/LinearAlgebra.fsx
@@ -77,3 +77,78 @@ let model (f:Featurizer)(data:Obs seq) =
     let theta = estimate(vector Yt)(matrix Xt)
     let predict = predictor f theta
     theta, predict
+
+let featurizer0 (obs:Obs) =
+    [1.;
+    float obs.Instant]
+
+let (theta0, model0) = model featurizer0 training
+
+evaluate model0 training |> printfn "Training: %.0f"
+evaluate model0 validation |> printfn "Validation: %.0f"
+
+let featurizer1 (obs:Obs) =
+    [1.;
+    float obs.Instant;
+    float obs.Atemp;
+    float obs.Hum;
+    float obs.Temp;
+    float obs.Windspeed]
+
+let (theta1, model1) = model featurizer1 training
+
+evaluate model1 training |> printfn "Training: %.0f"
+evaluate model1 validation |> printfn "Validation: %.0f"
+
+let featurizer2 (obs:Obs) =
+    [1.;
+    float obs.Instant;
+    float obs.Atemp;
+    float obs.Hum;
+    float obs.Temp;
+    float obs.Windspeed;
+    (if obs.Weekday = 0 then 0.0 else 1.0);
+    (if obs.Weekday = 1 then 0.0 else 1.0);
+    (if obs.Weekday = 2 then 0.0 else 1.0);
+    (if obs.Weekday = 3 then 0.0 else 1.0);
+    (if obs.Weekday = 4 then 0.0 else 1.0);
+    (if obs.Weekday = 5 then 0.0 else 1.0);]
+
+let (theta2, model2) = model featurizer2 training
+evaluate model2 training |> printfn "Training: %.0f"
+evaluate model2 validation |> printfn "Validation: %.0f"
+
+let featurizer3 (obs:Obs) =
+    [1.;
+    float obs.Temp;
+    obs.Temp*obs.Temp |> float
+    ]
+
+let (theta3, model3) = model featurizer3 data
+
+Chart.Combine[
+    Chart.Point [for obs in data -> obs.Temp, obs.Cnt]
+    Chart.Line [for obs in data -> obs.Temp, model3 obs]]
+
+let featurizer4 (obs:Obs) =
+    [1.;
+    float obs.Instant;
+    float obs.Atemp;
+    float obs.Hum;
+    float obs.Temp;
+    float obs.Windspeed;
+    obs.Temp * obs.Temp |> float;
+    (if obs.Weekday = 0 then 0.0 else 1.0);
+    (if obs.Weekday = 1 then 0.0 else 1.0);
+    (if obs.Weekday = 2 then 0.0 else 1.0);
+    (if obs.Weekday = 3 then 0.0 else 1.0);
+    (if obs.Weekday = 4 then 0.0 else 1.0);
+    (if obs.Weekday = 5 then 0.0 else 1.0);]
+
+let (theta4, model4) = model featurizer4 data
+evaluate model4 training |> printfn "Training: %.0f"
+evaluate model4 validation |> printfn "Validation: %.0f"
+
+Chart.Combine[
+    Chart.Line [for obs in data -> float obs.Cnt]
+    Chart.Line [for obs in data -> model4 obs]]
